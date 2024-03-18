3cc0b53a9837a379158bb8bfa548a119ac2bcd19
oscar <ordc120@gmail.com>
2024-03-18 18:49:45 +0100

    dark-mode,ligth-mode

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 "use strict";
 
+const drumPads = document.querySelectorAll(".drum-pad");
+const audios = document.querySelectorAll("audio");
 
-const botonModoColor = document.querySelector("#darkModeButton");
-const body = document.body;
+drumPads.forEach((pad) => {
+  pad.addEventListener("click", () => {
+    playSound(pad);
+  });
+});
 
-colorModeButton.addEventListener("click", cambiarModoColor);
+window.addEventListener("keydown", (e) => {
+  const key = e.keyCode.toString();
+  const pad = document.querySelector(`.drum-pad[data-key="${key}"]`);
+  if (pad) {
+    playSound(pad);
+  }
+});
 
-function cambiarModoColor() {
-    body.classList.toggle("darkMode");
-
-    if (body.classList.contains("darkMode")) {
-        colorModeButton.innerText = "Modo diurno";
-    } else {
-        colorModeButton.innerText ="Modo oscuro";
-    }
+function playSound(pad) {
+  const key = pad.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const light = document.getElementById("light-sun");
+  const dark = document.getElementById("dark-moon");
 
+  light.addEventListener("click", function () {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+  });
 
+  dark.addEventListener("click", function () {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+  });
+});
